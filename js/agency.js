1cb78932d430f6088d14fc7ec334cda97828a1a1
Kartik Lunkad <kartiklunkad@gmail.com>
2015-10-20 13:20:46 -0400

    msg

--- js/agency.js
+++ js/agency.js
@@ -65,6 +65,17 @@ $(function() {
     });
 });
 
+$(document).ready(function () {
+        $(document).on('show.bs.modal', '.modal', function (event) {
+            var zIndex = 1040 + (10 * $('.modal:visible').length);
+            $(this).css('z-index', zIndex);
+            setTimeout(function() {
+                $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+            }, 0);
+        });
+});
+
+
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
